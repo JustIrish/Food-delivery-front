ae8f852f0a8ac77d9d62d8735da618a1d30fa56b
Irina Khorunz <irahorunz87@gmail.com>
2023-06-02 13:09:03 +0300

    add routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+import { lazy } from 'react';
+import SharedLayout from './SharedLayout/SharedLayout';
+
+const Home = lazy(() => import('../pages/HomePage'));
+const Shops = lazy(() => import('../pages/ShopsPage'));
+const ShoppingCart = lazy(() => import('../pages/ShoppingCartPage'));
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/shops" element={<Shops />} />
+        <Route path="/shopping" element={<ShoppingCart />} />
+        <Route path="*" element={<Home />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
