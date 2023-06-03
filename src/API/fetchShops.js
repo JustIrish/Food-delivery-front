3c99a858abb524edada0ea8dc1d6d8d91e8c120a
Irina Khorunz <irahorunz87@gmail.com>
2023-06-03 15:50:44 +0300

    add sevices localStorage

--- src/API/fetchShops.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-axios.defaults.baseURL = 'https://food-delivery-backend-r64y.onrender.com';
-
-export const fetchShops = async () => {
-  const response = await axios.get(`/api/shops`);
-  return response.data;
-};
