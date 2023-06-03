5f555bae06575e4e85722a104b6b6f815deb5056
Irina Khorunz <irahorunz87@gmail.com>
2023-06-03 21:23:49 +0300

    Update ContactsForm.js

--- src/components/ContactsForm/ContactsForm.js
+++ src/components/ContactsForm/ContactsForm.js
@@ -1,5 +1,5 @@
 // import { useState } from 'react';
-import { Formik } from 'formik';
+import { Formik, ErrorMessage } from 'formik';
 
 import { schema } from 'common/validationSchema';
 
@@ -10,9 +10,9 @@ import {
   BtnStyled,
 } from './ContactsForm.styled';
 
-const ContactsForm = () => {
+const ContactsForm = ({ totalData }) => {
   const handleSubmit = (values, { resetForm }) => {
-    console.log(values);
+    console.log(values, totalData);
 
     resetForm();
   };
@@ -27,22 +27,27 @@ const ContactsForm = () => {
         <LabelStyled>
           Name:
           <InputStyled type="text" name="userName" />
+          <ErrorMessage name="userName" />
         </LabelStyled>
 
         <LabelStyled>
           Email:
           <InputStyled type="email" name="email" />
+          <ErrorMessage name="email" />
         </LabelStyled>
 
         <LabelStyled>
           Phone:
           <InputStyled type="tel" name="phone" />
+          <ErrorMessage name="phone" />
         </LabelStyled>
 
         <LabelStyled>
           Address:
           <InputStyled type="text" name="address" />
+          <ErrorMessage name="address" />
         </LabelStyled>
+        <div>Total cost: {totalData}</div>
         <BtnStyled type="submit">Send order</BtnStyled>
       </FormStyled>
     </Formik>
