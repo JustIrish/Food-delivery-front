495392820f1b298748b86dd698e5b6473d63f4b4
Irina Khorunz <irahorunz87@gmail.com>
2023-06-04 00:29:07 +0300

    Create Loader.js

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,17 @@
+import { Oval } from 'react-loader-spinner';
+import { SpinnerWrapper } from './Loader.styled';
+
+export const Loader = () => (
+  <SpinnerWrapper>
+    <Oval
+      height={14}
+      width={14}
+      color="#000000"
+      visible={true}
+      ariaLabel="oval-loading"
+      secondaryColor="#84a17d"
+      strokeWidth={10}
+      strokeWidthSecondary={10}
+    />
+  </SpinnerWrapper>
+);
