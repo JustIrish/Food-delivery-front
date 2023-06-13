466da667042487a8026e8ef356a53ddc4cfea209
Irina Khorunz <irahorunz87@gmail.com>
2023-06-13 20:11:25 +0300

    add corections

--- src/components/SharedLayout/SharedLayout.styled.js
+++ src/components/SharedLayout/SharedLayout.styled.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export const Layout = styled.div`
-  max-width: 1280px;
   margin: 0 auto;
 `;
 
--- src/pages/Pages.styled.js
+++ src/pages/Pages.styled.js
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
 export const Wrapper = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
   padding: 100px 16px;
   display: flex;
   align-items: flex-start;
@@ -10,12 +12,12 @@ export const Wrapper = styled.div`
 
 export const HomeTextWrap = styled.div`
   width: 800px;
-  height: 40vh;
+  height: 350px;
   padding: 100px 150px 24px 50px;
-  background-color: #f3f7f2;
+  background-color: rgba(243, 247, 242, 0.3);
   border-bottom-right-radius: 70%;
   border-top-left-radius: 30%;
-  box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 20px, rgba(0, 0, 0, 0.15) 0px 6px 6px;
 `;
 
 export const HistoryWrap = styled.div`
